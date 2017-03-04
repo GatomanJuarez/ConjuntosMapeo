38ccfcf6bdc1def9984aa61e17d16e9a539a9965
Gatoman <juarez_devora_ja@hotmail.com>
2017-03-04 13:27:49 -0800

    Listas

--- ConjuntosMapeo/Form1.Designer.cs
+++ ConjuntosMapeo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConjuntosMapeo
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnUniverso = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -54,46 +55,49 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Ingresa los siguientes valores.";
             // 
-            // button1
+            // btnUniverso
             // 
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(47, 40);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(85, 28);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Universo";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnUniverso.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUniverso.Location = new System.Drawing.Point(47, 40);
+            this.btnUniverso.Name = "btnUniverso";
+            this.btnUniverso.Size = new System.Drawing.Size(85, 28);
+            this.btnUniverso.TabIndex = 1;
+            this.btnUniverso.Text = "Universo";
+            this.btnUniverso.UseVisualStyleBackColor = true;
+            this.btnUniverso.Click += new System.EventHandler(this.btnUniverso_Click);
             // 
-            // button2
+            // btnA
             // 
-            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(151, 40);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 28);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "A";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnA.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnA.Location = new System.Drawing.Point(151, 40);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(85, 28);
+            this.btnA.TabIndex = 2;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = true;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
             // 
-            // button3
+            // btnB
             // 
-            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(47, 74);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(85, 28);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "B";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnB.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnB.Location = new System.Drawing.Point(47, 74);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(85, 28);
+            this.btnB.TabIndex = 3;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = true;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
             // 
-            // button4
+            // btnC
             // 
-            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(151, 74);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(85, 28);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "C*";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnC.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC.Location = new System.Drawing.Point(151, 74);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(85, 28);
+            this.btnC.TabIndex = 4;
+            this.btnC.Text = "C*";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
             // 
             // label2
             // 
@@ -185,7 +189,7 @@
             this.label10.TabIndex = 12;
             this.label10.Text = "C";
             // 
-            // Form1
+            // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -199,13 +203,15 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btnUniverso);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Programa.";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -215,10 +221,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnUniverso;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
--- ConjuntosMapeo/Form1.cs
+++ ConjuntosMapeo/Form1.cs
@@ -12,9 +12,9 @@ using InputKey;
 
 namespace ConjuntosMapeo
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
@@ -24,20 +24,55 @@ namespace ConjuntosMapeo
         List<String> mundoA = new List<String>();
         List<String> mundoB = new List<String>();
         List<String> mundoC = new List<String>();
+        String auxiliar;
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnUniverso_Click(object sender, EventArgs e)
         {
+            auxiliar = "";
             //Guardamos en la variable el valor introducido por el usuario.
-            string auxiliar = InputDialog.mostrar("Introduzca algun dato");
+            auxiliar = InputDialog.mostrar("Introduzca algun dato");
             //Lo guardamos en nuestra lista correspondiente.
             universo.Add(auxiliar);
             //Lo imprimimos para ver el valor.
             label4.Text += auxiliar + "\n";
+        }
+
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            auxiliar = "";
+            //Guardamos en la variable el valor introducido por el usuario.
+            auxiliar = InputDialog.mostrar("Introduzca algun dato");
+            //Lo guardamos en nuestra lista correspondiente.
+            mundoA.Add(auxiliar);
+            //Lo imprimimos para ver el valor.
+            label5.Text += auxiliar + "\n";
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            auxiliar = "";
+            //Guardamos en la variable el valor introducido por el usuario.
+            auxiliar = InputDialog.mostrar("Introduzca algun dato");
+            //Lo guardamos en nuestra lista correspondiente.
+            mundoB.Add(auxiliar);
+            //Lo imprimimos para ver el valor.
+            label7.Text += auxiliar + "\n";
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            auxiliar = "";
+            //Guardamos en la variable el valor introducido por el usuario.
+            auxiliar = InputDialog.mostrar("Introduzca algun dato");
+            //Lo guardamos en nuestra lista correspondiente.
+            mundoC.Add(auxiliar);
+            //Lo imprimimos para ver el valor.
+            label9.Text += auxiliar + "\n";
         }
     }
 }
